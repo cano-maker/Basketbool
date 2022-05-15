74694975d3bd1e3aab5f14d733581551fd850ea4
Fabio Cano <fabiocano894@gmail.com>
2022-05-15 10:55:49 -0500

    create repositori

---
+++ Basketbool.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Basketbool.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
