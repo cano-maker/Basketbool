60962aac101a2a58be9da173d17d5bc24972abec
Fabio Cano <fabiocano894@gmail.com>
2022-05-16 10:56:27 -0500

    add create view

--- Basketbool.Web/Controllers/TeamsController.cs
+++ Basketbool.Web/Controllers/TeamsController.cs
@@ -1,4 +1,6 @@
 ﻿using Basketbool.Web.Data;
+using Basketbool.Web.Data.Entities;
+using Basketbool.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
@@ -18,5 +20,45 @@ namespace Basketbool.Web.Controllers
         {
             return View(await _context.Teams.ToListAsync());
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create(TeamViewModel teamViewModel)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        string path = string.Empty;
+
+        //        if (teamViewModel.LogoFile != null)
+        //        {
+        //            path = await _imageHelper.UploadImageAsync(teamViewModel.LogoFile, "Teams");
+        //        }
+        //        TeamEntity teamEntity = _converterHelper.ToTeamEntity(teamViewModel, path, true);
+        //        _context.Add(teamEntity);
+        //        try
+        //        {
+        //            await _context.SaveChangesAsync();
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            if (ex.InnerException.Message.Contains("duplicate"))
+        //            {
+        //                ModelState.AddModelError(string.Empty, $"Already exist the team: {teamEntity.Name}.");
+        //            }
+        //            else
+        //            {
+        //                ModelState.AddModelError(string.Empty, ex.InnerException.Message);
+        //            }
+
+        //        }
+        //    }
+        //    return View(teamViewModel);
+        //}
     }
 }
---
+++ Basketbool.Web/Models/TeamViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Basketbool.Web.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace Basketbool.Web.Models
+{
+    public class TeamViewModel : TeamEntity
+    {
+
+        [Display(Name = "Logo")]
+        public IFormFile LogoFile { get; set; }
+
+    }
+}
