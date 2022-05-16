1797f5aa4f6e8f5fc866ed3b8b01134ba1aead3d
Fabio Cano <fabiocano894@gmail.com>
2022-05-16 10:30:30 -0500

    add index for teams

---
+++ Basketbool.Web/Controllers/TeamsController.cs
@@ -0,0 +1,22 @@
+﻿using Basketbool.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Basketbool.Web.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public TeamsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Teams.ToListAsync());
+        }
+    }
+}
