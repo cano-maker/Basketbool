8d843add88de9fcf0f8ef40318954335103b1c0d
Fabio Cano <fabiocano894@gmail.com>
2022-05-18 14:17:03 -0500

    add index for seasons

---
+++ Basketbool.Web/Controllers/PredictionsController.cs
@@ -0,0 +1,18 @@
+﻿using Basketbool.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basketbool.Web.Controllers
+{
+    public class PredictionsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public PredictionsController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+
+    }
+}
---
+++ Basketbool.Web/Controllers/SeasonsController.cs
@@ -0,0 +1,28 @@
+﻿using Basketbool.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basketbool.Web.Controllers
+{
+    public class SeasonsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public SeasonsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context
+                .Seasons
+                .Include(t => t.MatchDays)
+                .OrderBy(t => t.StartDate)
+                .ToListAsync());
+        }
+
+    }
+}
